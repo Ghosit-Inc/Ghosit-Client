2e3b9210bf863d71299594c51e599b5ec8030c83
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:55:27 +0100

    feat: categories

---
+++ src/main/java/de/constt/nexsus_client/client/roots/implementations/CategoryImplementation.java
@@ -0,0 +1,12 @@
+package de.constt.nexsus_client.client.roots.implementations;
+
+public class CategoryImplementation {
+    public enum Categories {
+        COMBAT,
+        MOVEMENT,
+        RENDER,
+        MISC,
+        PLAYER,
+        EXPLOITS
+    }
+}
