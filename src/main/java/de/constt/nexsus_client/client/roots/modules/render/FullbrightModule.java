3770d51d3ecc17cd43677f998fcca168ba769532
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:56:52 +0100

    feat: module info

--- src/main/java/de/constt/nexsus_client/client/roots/modules/render/FullbrightModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/render/FullbrightModule.java
@@ -1,16 +1,18 @@
-package de.constt.nexsus_client.client.roots.modules;
+package de.constt.nexsus_client.client.roots.modules.render;
 
+import de.constt.nexsus_client.client.annotations.InfoAnnotation;
+import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 import net.minecraft.network.packet.Packet;
 
+@InfoAnnotation(
+        name = "Fullbright",
+        description = "Changes the gamma to be always max bright",
+        category = CategoryImplementation.Categories.RENDER
+)
 public class FullbrightModule extends ModuleImplementation {
     @Override
     public boolean modifyPacket(Packet<?> packet) {
         return false;
-    }
-
-    @Override
-    public void onTick() {
-
     }
 }
