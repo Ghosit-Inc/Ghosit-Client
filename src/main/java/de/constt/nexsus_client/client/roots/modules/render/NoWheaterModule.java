5e539f44df7a01afca2c80ea782f07e688185177
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:57:01 +0100

    feat: module info

--- src/main/java/de/constt/nexsus_client/client/roots/modules/render/NoWheaterModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/render/NoWheaterModule.java
@@ -1,17 +1,19 @@
-package de.constt.nexsus_client.client.roots.modules;
+package de.constt.nexsus_client.client.roots.modules.render;
 
+import de.constt.nexsus_client.client.annotations.InfoAnnotation;
+import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 import net.minecraft.network.packet.Packet;
 
+@InfoAnnotation(
+        name = "No Wheater",
+        description = "Toggle specific Wheater",
+        category = CategoryImplementation.Categories.RENDER
+)
 public class NoWheaterModule extends ModuleImplementation {
 
     @Override
     public boolean modifyPacket(Packet<?> packet) {
         return false;
-    }
-
-    @Override
-    public void onTick() {
-
     }
 }
