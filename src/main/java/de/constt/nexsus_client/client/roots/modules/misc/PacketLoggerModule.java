1940f32fb0d8747b1d1f64e5e86646c83d6ef9cb
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:56:02 +0100

    feat: log packets to chat

--- src/main/java/de/constt/nexsus_client/client/roots/modules/misc/PacketLoggerModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/misc/PacketLoggerModule.java
@@ -1,17 +1,25 @@
-package de.constt.nexsus_client.client.roots.modules;
+package de.constt.nexsus_client.client.roots.modules.misc;
 
+import de.constt.nexsus_client.client.annotations.InfoAnnotation;
+import de.constt.nexsus_client.client.helperFunctions.chatHelperFunction;
+import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 import net.minecraft.network.packet.Packet;
 
+@InfoAnnotation(
+        name = "Packet Logger",
+        description = "Logs packets in chat",
+        category = CategoryImplementation.Categories.MISC
+)
 public class PacketLoggerModule extends ModuleImplementation {
 
     @Override
     public boolean modifyPacket(Packet<?> packet) {
+        if (!enabled) return false;
+
+        String packetInfo = packet.getClass().getSimpleName();
+        chatHelperFunction.sendCSMessageNeutral("Packet sent/received: " + packetInfo);
+
         return false;
-    }
-
-    @Override
-    public void onTick() {
-
     }
 }
