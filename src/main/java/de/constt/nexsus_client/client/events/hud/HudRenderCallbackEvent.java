906cf4d396f5fc34bf658fe6f018e798b3f6ebe0
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:53:21 +0100

    feat: render hud

---
+++ src/main/java/de/constt/nexsus_client/client/events/hud/HudRenderCallbackEvent.java
@@ -0,0 +1,16 @@
+package de.constt.nexsus_client.client.events.hud;
+
+import de.constt.nexsus_client.client.helperFunctions.renderHelperFunction;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+
+public class HudRenderCallbackEvent {
+
+    public static void register() {
+        HudRenderCallback.EVENT.register((drawContext, delta) -> {
+            if (MinecraftClient.getInstance().getDebugHud().shouldShowDebugHud()) return;
+
+            renderHelperFunction.renderHud(drawContext);
+        });
+    }
+}
