0e3aeb33c077e62c61e8bb6e6b4f5e4407dc94c6
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:57:07 +0100

    feat: no fall

--- src/main/java/de/constt/nexsus_client/client/roots/modules/world/NoFallModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/world/NoFallModule.java
@@ -1,17 +1,38 @@
-package de.constt.nexsus_client.client.roots.modules;
+package de.constt.nexsus_client.client.roots.modules.world;
 
+import de.constt.nexsus_client.client.annotations.InfoAnnotation;
+import de.constt.nexsus_client.client.helperFunctions.chatHelperFunction;
+import de.constt.nexsus_client.client.mixins.PlayerMoveC2SPacketAccessor;
+import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
+@InfoAnnotation(
+        name = "No Fall",
+        description = "Disables fall damage",
+        category = CategoryImplementation.Categories.MISC
+)
 public class NoFallModule extends ModuleImplementation {
-
     @Override
     public boolean modifyPacket(Packet<?> packet) {
+        if (!(packet instanceof PlayerMoveC2SPacket)) return false;
+
+        var player = MinecraftClient.getInstance().player;
+        if (player == null) return false;
+
+        if (player.getAbilities().flying) {
+            ((PlayerMoveC2SPacketAccessor) packet).setOnGround(true);
+        }
+        else {
+            // Allow Elytra Flying
+            if (player.isFallFlying()) return false;
+            // Don't kill the player when NoFall is turned on too late (cancel fall damage)
+            if (player.getVelocity().getY() > -0.5) return false;
+            ((PlayerMoveC2SPacketAccessor) packet).setOnGround(true);
+        }
+
         return false;
-    }
-
-    @Override
-    public void onTick() {
-
     }
 }
