1cfe95f9d4ddfb266167d6841cf5cb449fe7a911
constt <175602385+constttdev@users.noreply.github.com>
2025-12-18 16:44:01 +0100

    feat: command annotation

---
+++ src/main/java/de/constt/nexsus_client/client/annotations/CommandAnnotation.java
@@ -0,0 +1,10 @@
+package de.constt.nexsus_client.client.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CommandAnnotation {
+    String name();
+    String description();
+}
