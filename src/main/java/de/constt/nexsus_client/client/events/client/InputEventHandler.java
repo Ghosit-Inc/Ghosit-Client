b38dc3ec443f67bb832ac762d0cded7d82049f5c
constt <175602385+constttdev@users.noreply.github.com>
2025-12-20 00:10:34 +0100

    feat: screen check

--- src/main/java/de/constt/nexsus_client/client/events/client/InputEventHandler.java
+++ src/main/java/de/constt/nexsus_client/client/events/client/InputEventHandler.java
@@ -1,9 +1,12 @@
 package de.constt.nexsus_client.client.events.client;
 
+import de.constt.nexsus_client.client.roots.gui.ModulesScreen;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 import de.constt.nexsus_client.client.roots.modules.ModuleManager;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screen.Screen;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.HashMap;
@@ -18,6 +21,7 @@ public class InputEventHandler {
             long window = MinecraftClient.getInstance().getWindow().getHandle();
 
             for (ModuleImplementation module : ModuleManager.getModules()) {
+                Screen screen = MinecraftClient.getInstance().currentScreen;
                 int key = module.keyBindingCode;
 
                 if (key <= 0) continue;
@@ -26,6 +30,8 @@ public class InputEventHandler {
                 boolean wasPressed = keyStates.getOrDefault(module, false);
 
                 if (isPressed && !wasPressed) {
+                    if(screen instanceof ChatScreen || screen instanceof ModulesScreen) return;
+
                     module.toggle();
                 }
 
