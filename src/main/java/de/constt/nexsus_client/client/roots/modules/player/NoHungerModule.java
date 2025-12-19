7bbac2cc54f75d7e18b201951456f380f2fb16a0
constt <175602385+constttdev@users.noreply.github.com>
2025-12-20 00:15:32 +0100

    feat: annotation rename rm: unused imports

--- src/main/java/de/constt/nexsus_client/client/roots/modules/player/NoHungerModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/player/NoHungerModule.java
@@ -1,20 +1,16 @@
 package de.constt.nexsus_client.client.roots.modules.player;
 
-import de.constt.nexsus_client.client.Nexsus_clientClient;
-import de.constt.nexsus_client.client.annotations.InfoAnnotation;
-import de.constt.nexsus_client.client.helperFunctions.chatHelperFunction;
+import de.constt.nexsus_client.client.annotations.ModuleInfoAnnotation;
 import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.data.DataGenerator;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
-@InfoAnnotation(
-        name = "No-Hunger",
+@ModuleInfoAnnotation(
+        name = "No Hunger",
         description = "Reduces the hunger taken by e.g. sprinting (Does not remove hunger)",
-        category = CategoryImplementation.Categories.PLAYER
+        category = CategoryImplementation.Categories.PLAYER,
+        internalModuleName = "nohunger"
 )
 public class NoHungerModule extends ModuleImplementation {
     @Override
