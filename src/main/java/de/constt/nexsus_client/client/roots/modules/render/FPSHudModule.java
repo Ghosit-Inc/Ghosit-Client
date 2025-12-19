671f4585a2dbe22927c919f47a455dd5edde1bd3
constt <175602385+constttdev@users.noreply.github.com>
2025-12-20 00:15:38 +0100

    feat: annotation rename

--- src/main/java/de/constt/nexsus_client/client/roots/modules/render/FPSHudModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/render/FPSHudModule.java
@@ -1,14 +1,15 @@
 package de.constt.nexsus_client.client.roots.modules.render;
 
 
-import de.constt.nexsus_client.client.annotations.InfoAnnotation;
+import de.constt.nexsus_client.client.annotations.ModuleInfoAnnotation;
 import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 
-@InfoAnnotation(
-        name = "FPS-(Hud)",
+@ModuleInfoAnnotation(
+        name = "FPS (Hud)",
         description = "Shows your FPS in the HUD",
-        category = CategoryImplementation.Categories.RENDER
+        category = CategoryImplementation.Categories.RENDER,
+        internalModuleName = "fpshud"
 )
 public class FPSHudModule extends ModuleImplementation {
 }
