6e8482e139f57d9453e81bae9720adec797c4faf
constt <175602385+constttdev@users.noreply.github.com>
2026-01-09 23:20:13 +0100

    feat: tested out the window icon function

--- src/main/java/de/constt/ghosit_client/client/Ghosit_clientClient.java
+++ src/main/java/de/constt/ghosit_client/client/Ghosit_clientClient.java
@@ -10,11 +10,25 @@ import de.constt.ghosit_client.client.events.player.ClientPlayerConnectionEvents
 import de.constt.ghosit_client.client.events.render.ScreenRenderCallbackEvent;
 import de.constt.ghosit_client.client.helperFunctions.ChatHelperFunction;
 import de.constt.ghosit_client.client.helperFunctions.ConfigManagerHelperFunction;
+import de.constt.ghosit_client.client.helperFunctions.WindowHelperFunction;
 import de.constt.ghosit_client.client.roots.commands.CommandManager;
 import de.constt.ghosit_client.client.roots.modules.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.texture.NativeImage;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWImage;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import static de.constt.ghosit_client.client.helperFunctions.WindowHelperFunction.setWindowIcon;
 
 public class Ghosit_clientClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("ghosit_client");
@@ -51,4 +65,29 @@ public class Ghosit_clientClient implements ClientModInitializer {
         // Temporarily toggle Modules
         ModuleManager.tempToggleModules();
     }
+
+    /*
+    public static void setWindowIcon(String pathInJar) throws IOException {
+        try (
+                InputStream inputStream = MinecraftClient.class.getResourceAsStream(pathInJar);
+                MemoryStack stack = MemoryStack.stackPush()
+        ) {
+            if (inputStream == null) throw new IOException("missing: " + pathInJar);
+            NativeImage image = NativeImage.read(inputStream);
+            ByteBuffer pixelbuffer = MemoryUtil.memAlloc(image.getWidth() * image.getHeight() * 4);
+            pixelbuffer.asIntBuffer().put(image.copyPixelsRgba());
+
+            GLFWImage.Buffer buf = GLFWImage.malloc(1, stack);
+            buf.position(0);
+            buf.width(image.getWidth());
+            buf.height(image.getHeight());
+            buf.pixels(pixelbuffer);
+
+            GLFW.glfwSetWindowIcon(MinecraftClient.getInstance().getWindow().getHandle(), buf.position(0));
+            image.close();
+            MemoryUtil.memFree(pixelbuffer);
+        }
+    }
+
+     */
 }
