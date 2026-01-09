17a60eb81753752389687ddf91627bd0359980b8
constt <175602385+constttdev@users.noreply.github.com>
2026-01-09 22:26:47 +0100

    feat: commmand tick

---
+++ src/main/java/de/constt/ghosit_client/client/events/client/ClientTickEventsEvent.java
@@ -0,0 +1,43 @@
+package de.constt.ghosit_client.client.events.client;
+
+import de.constt.ghosit_client.client.config.GhositConfigData;
+import de.constt.ghosit_client.client.helperFunctions.ChatHelperFunction;
+import de.constt.ghosit_client.client.roots.commands.CommandManager;
+import de.constt.ghosit_client.client.roots.modules.ModuleManager;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+public class ClientTickEventsEvent {
+    public static void register() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            String clientVersion = GhositConfigData.getConfigValue("clientVersion");
+            if (client.getWindow() != null) {
+                boolean watermark = Boolean.parseBoolean(
+                        GhositConfigData.getConfigValue("watermark")
+                );
+                if(watermark) client.getWindow().setTitle(ChatHelperFunction.getPrefix(true) +" Client "+clientVersion+" | MC 1.21.1 Fabric");
+            }
+        });
+
+        ClientTickEvents.START_CLIENT_TICK.register(client ->
+                ModuleManager.getModules().forEach(module -> {
+                    if (module.getEnabledStatus())
+                        module.tick();
+                })
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client ->
+                ModuleManager.getModules().forEach(module -> {
+                    if (module.getEnabledStatus())
+                        module.postTick();
+                })
+        );
+
+        ClientTickEvents.START_CLIENT_TICK.register(client ->
+                CommandManager.getCommands().forEach(command -> {
+                    if (command.getEnabledStatus()) {
+                        command.tick();
+                    }
+                })
+        );
+    }
+}
