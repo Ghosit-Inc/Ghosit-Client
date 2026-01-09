1e1c33594194f42b45231d6d0a8ad10a1753aa2e
constt <175602385+constttdev@users.noreply.github.com>
2026-01-09 23:20:30 +0100

    feat: remade main file

--- src/main/java/de/constt/ghosit_client/Ghosit_client.java
+++ src/main/java/de/constt/ghosit_client/Ghosit_client.java
@@ -2,8 +2,11 @@ package de.constt.ghosit_client;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Ghosit_client implements ModInitializer {
+import java.io.IOException;
 
+import static de.constt.ghosit_client.client.helperFunctions.WindowHelperFunction.setWindowIcon;
+
+public class Ghosit_client implements ModInitializer {
     @Override
     public void onInitialize() {
     }
