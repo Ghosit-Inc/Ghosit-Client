247b4b8a4322b7285fef8b9cf82d0f18476172e6
constt <175602385+constttdev@users.noreply.github.com>
2026-01-10 23:48:21 +0100

    feat: checks

--- src/main/java/de/constt/ghosit_client/client/roots/modules/misc/ClickGUIModule.java
+++ src/main/java/de/constt/ghosit_client/client/roots/modules/misc/ClickGUIModule.java
@@ -5,6 +5,11 @@ import de.constt.ghosit_client.client.roots.gui.ModulesScreen;
 import de.constt.ghosit_client.client.roots.implementations.CategoryImplementation;
 import de.constt.ghosit_client.client.roots.implementations.ModuleImplementation;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.multiplayer.AddServerScreen;
+import net.minecraft.client.gui.screen.multiplayer.DirectConnectScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 
 import java.util.Objects;
 
@@ -21,9 +26,11 @@ public class ClickGUIModule extends ModuleImplementation {
         super.onEnable();
 
         MinecraftClient client = MinecraftClient.getInstance();
-        if(!Objects.equals(client.currentScreen, new ModulesScreen("Ghosit Client Modules Screen"))) {
-            client.setScreen(new ModulesScreen("Ghosit Client Modules Screen"));
-        }
+        if(!(client.currentScreen instanceof MultiplayerScreen || client.currentScreen instanceof AddServerScreen || client.currentScreen instanceof DirectConnectScreen)) {
+            if(!Objects.equals(client.currentScreen, new ModulesScreen("Ghosit Client Modules Screen"))) {
+                client.setScreen(new ModulesScreen("Ghosit Client Modules Screen"));
+            }
+        } else return;
         // this.toggle(); - crashes on toggle
     }
 }
