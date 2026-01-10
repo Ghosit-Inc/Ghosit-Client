b64f51d079c6f1b0ebdc165ece31053500bb077c
constt <175602385+constttdev@users.noreply.github.com>
2026-01-10 15:18:39 +0100

    feat: mushroom farm macro

---
+++ src/main/java/de/constt/ghosit_client/client/roots/macros/skyblock/MushroomFarmMacro.java
@@ -0,0 +1,11 @@
+package de.constt.ghosit_client.client.roots.macros.skyblock;
+
+import de.constt.ghosit_client.client.roots.implementations.MacroImplementation;
+
+public class MushroomFarmMacro extends MacroImplementation {
+    @Override
+    public void execute() {
+
+        super.execute();
+    }
+}
