22fe7f926b0e8c29ce48af7554400e3ba13b1117
constt <175602385+constttdev@users.noreply.github.com>
2026-01-11 16:57:32 +0100

    feat: settings for module

--- src/main/java/de/constt/ghosit_client/client/roots/modules/misc/SpammerModule.java
+++ src/main/java/de/constt/ghosit_client/client/roots/modules/misc/SpammerModule.java
@@ -3,15 +3,28 @@ package de.constt.ghosit_client.client.roots.modules.misc;
 import de.constt.ghosit_client.client.annotations.ModuleInfoAnnotation;
 import de.constt.ghosit_client.client.roots.implementations.CategoryImplementation;
 import de.constt.ghosit_client.client.roots.implementations.ModuleImplementation;
+import de.constt.ghosit_client.client.roots.settings.BooleanSettingImplementation;
+import de.constt.ghosit_client.client.roots.settings.NumberSettingImplementation;
+import de.constt.ghosit_client.client.roots.settings.StringSettingImplementation;
 import net.minecraft.client.MinecraftClient;
 
 @ModuleInfoAnnotation(
         name = "Spammer",
-        description = "Automaticly sends messages from your account to chat",
+        description = "Automatically sends messages from your account to chat",
         category = CategoryImplementation.Categories.MISC,
         internalModuleName = "spammer"
 )
 public class SpammerModule extends ModuleImplementation {
+    StringSettingImplementation message;
+    NumberSettingImplementation tickDelay;
+
+    public SpammerModule() {
+        message = new StringSettingImplementation("MESSAGE", "MESSAGE");
+        tickDelay = new NumberSettingImplementation("TICKS-DELAY", 4);
+
+        registerSetting(message);
+        registerSetting(tickDelay);
+    }
 
     private int tickCounter = 0;
 
@@ -23,9 +36,9 @@ public class SpammerModule extends ModuleImplementation {
             return;
         }
 
-        if (++tickCounter >= 4) {
+        if (++tickCounter >= tickDelay.get()) {
             tickCounter = 0;
-            mc.player.networkHandler.sendChatMessage("Ghosit client currently free!");
+            mc.player.networkHandler.sendChatMessage(message.get());
         }
 
         super.tick();
